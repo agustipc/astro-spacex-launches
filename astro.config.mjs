6f9f815eeb885dfa3d5b007921bd557267888b17
Agustí Pou Company <agustipcom@gmail.com>
2023-10-24 13:01:25 +0200

    Add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import nodejs from '@astrojs/node'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
+  adapter: nodejs({
+    mode: 'middleware'
+  }),
   output: 'hybrid'
-});
+})
